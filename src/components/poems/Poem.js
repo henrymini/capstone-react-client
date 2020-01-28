b998f8aaaeaf46e1f1a097c21526d03e4e7a9715
RWPicklechips <hminicucci@gmail.com>
2020-01-28 09:22:25 -0500

    Poem handler commit
    - added Poem.js and imported react and axios
    - added GET all functionality
    - need to change the .catch response

---
+++ src/components/poems/Poem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+
+class Poem extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      poem: null
+    }
+  }
+  componentDidMount () {
+    axios({
+      url: `${apiUrl}/poems/${this.props.match.params.id}`,
+      method: 'GET'
+    })
+      .then(res => {
+        this.setState({ poem: res.data.poem })
+      })
+      .catch(console.error)
+  }
+}
+
+export default Poem
