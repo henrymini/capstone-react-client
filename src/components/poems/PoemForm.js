7e943065db770fdcb0c930c38119c7e16f2f4ad3
RWPicklechips <hminicucci@gmail.com>
2020-01-28 10:17:48 -0500

    PoemForm commit
    - created PoemForm.js for poem submission
    - added changing text in  header and submit button based on page status
    - forget to add form to PoemCreate

---
+++ src/components/poems/PoemForm.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const PoemForm = ({ poem, handleChange, handleSubmit }) => (
+  <div>
+    <h1>{poem._id ? 'Update' : 'Create a new Poem'}</h1>
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Title"
+        type="text"
+        value={poem.title}
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        placeholder="Author"
+        type="text"
+        value={poem.author}
+        onChange={handleChange}
+        name="author"
+      />
+      <input
+        placeholder="Enter your Poem Here"
+        type="text"
+        value={poem.body}
+        onChange={handleChange}
+        name="body"
+      />
+      <input
+        placeholder="Year of Release"
+        type="number"
+        value={poem.year}
+        onChange={handleChange}
+        name="year"
+      />
+      <button type="submit" className="btn">{poem._id ? 'Update' : 'Add'}</button>
+      <Link to={poem._id ? `/poems/${poem._id}` : '/'} className="btn btn-warning">Cancel</Link>
+    </form>
+  </div>
+)
+
+export default PoemForm
