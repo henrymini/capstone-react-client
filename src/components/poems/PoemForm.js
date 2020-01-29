ebb2622d6c3c66b6104a2aa4fc7c26f6377f3ad4
RWPicklechips <hminicucci@gmail.com>
2020-01-29 15:21:28 -0500

    Second coat of paint
    - added background color to forms
    - gave the displays a 50px margin
    - centered background image

--- src/components/poems/PoemForm.js
+++ src/components/poems/PoemForm.js
@@ -3,43 +3,45 @@ import { Link } from 'react-router-dom'
 
 const PoemForm = ({ poem, handleChange, handleSubmit }) => (
   <div className="row">
-    <h1>{poem._id ? 'Update' : 'Enter Poem Information'}</h1>
-    <form onSubmit={handleSubmit}>
-      <input
-        className="form mb-2"
-        placeholder="Title"
-        type="text"
-        value={poem.title}
-        onChange={handleChange}
-        name="title"
-      />
-      <input
-        className="form mb-2"
-        placeholder="Author"
-        type="text"
-        value={poem.author}
-        onChange={handleChange}
-        name="author"
-      />
-      <input
-        className="form mb-2"
-        placeholder="Enter your Poem Here"
-        type="text area"
-        value={poem.body}
-        onChange={handleChange}
-        name="body"
-      />
-      <input
-        className="form mb-2"
-        placeholder="Year of Release"
-        type="number"
-        value={poem.year}
-        onChange={handleChange}
-        name="year"
-      />
-      <button type="submit" className="btn btn-danger mr-2">{poem._id ? 'Update' : 'Add'}</button>
-      <Link to={poem._id ? `/poems/${poem._id}` : '/'} className="btn btn-secondary">Back</Link>
-    </form>
+    <div className="col-sm-10 col-md-8 mx-auto mt-5">
+      <h1>{poem._id ? 'Update' : 'Enter Poem Information'}</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form mb-2"
+          placeholder="Title"
+          type="text"
+          value={poem.title}
+          onChange={handleChange}
+          name="title"
+        />
+        <input
+          className="form mb-2"
+          placeholder="Author"
+          type="text"
+          value={poem.author}
+          onChange={handleChange}
+          name="author"
+        />
+        <input
+          className="form mb-2"
+          placeholder="Enter your Poem Here"
+          type="text area"
+          value={poem.body}
+          onChange={handleChange}
+          name="body"
+        />
+        <input
+          className="form mb-2"
+          placeholder="Year of Release"
+          type="number"
+          value={poem.year}
+          onChange={handleChange}
+          name="year"
+        />
+        <button type="submit" className="btn btn-danger mr-2">{poem._id ? 'Update' : 'Add'}</button>
+        <Link to={poem._id ? `/poems/${poem._id}` : '/'} className="btn btn-secondary">Back</Link>
+      </form>
+    </div>
   </div>
 )
 
