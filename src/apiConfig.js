8094a41043ce9156d02758825546294e78880cfa
RWPicklechips <hminicucci@gmail.com>
2020-01-29 12:00:59 -0500

    Production api correction
    - added correct api url to apiConfig

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://quiet-retreat-06599.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
