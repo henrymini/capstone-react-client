398c1cbd568164d4adf54cd82bf184fdd6c2b8ac
RWPicklechips <hminicucci@gmail.com>
2020-01-29 09:49:52 -0500

    Poem DELETE functionality
    - added deleted boolean to Poem component
    - added delete handler with success and failure protocol and messages
    - still need delete redirection in render

--- src/components/poems/Poem.js
+++ src/components/poems/Poem.js
@@ -6,7 +6,8 @@ class Poem extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      poem: null
+      poem: null,
+      deleted: false
     }
   }
   componentDidMount () {
@@ -22,6 +23,26 @@ class Poem extends Component {
         this.setState({ poem: res.data.poem })
       })
       .catch(console.error)
+  }
+  handleDelete = () => {
+    axios({
+      url: `${apiUrl}/poems/${this.props.match.params.id}`,
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token token=${this.props.user.token}`
+      }
+    })
+      .then(() => this.props.alert({
+        heading: 'We can forget about that',
+        message: `${this.state.poem.title} has been deleted`,
+        variant: 'success'
+      }))
+      .then(this.setState({ deleted: true }))
+      .catch(() => this.props.alert({
+        heading: 'Something Goofed™',
+        message: 'The poem was not deleted',
+        variant: 'danger'
+      }))
   }
   render () {
     return (
