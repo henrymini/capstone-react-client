2fd6a2079e424fb447f14d935e158f12d39ccc7a
RWPicklechips <hminicucci@gmail.com>
2020-01-30 13:32:53 -0500

    Owner addition client
    - added owner to Poem.js
    - edit + delete buttons will not appear for poems not owned by the user

--- src/components/poems/Poem.js
+++ src/components/poems/Poem.js
@@ -66,10 +66,14 @@ class Poem extends Component {
         <p>{this.state.poem.body}</p>
         {this.state.poem.year && <p>Released {this.state.poem.year}</p>}
         <div className="poem-buttons">
-          <React.Fragment>
-            <Link className="btn btn-danger mr-2" to={`/poems/${this.props.match.params.id}/edit`}>Edit</Link>
-            <button className="btn btn-warning mr-2" onClick={this.handleDelete}>Delete</button>
-          </React.Fragment>
+          {this.props.user && (this.props.user._id === this.state.book.owner._id) &&
+            (
+              <React.Fragment>
+                <Link className="btn btn-danger mr-2" to={`/poems/${this.props.match.params.id}/edit`}>Edit</Link>
+                <button className="btn btn-warning mr-2" onClick={this.handleDelete}>Delete</button>
+              </React.Fragment>
+            )
+          }
           <Link className="btn btn-secondary" to="/">Back</Link>
         </div>
       </div>
